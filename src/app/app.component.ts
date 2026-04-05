d6e79c1e62336b9eb84d9c7d9b48ed5f8bc14555
Antonio <a.augusto03@live.com>
2026-04-05 12:50:22 -0300

    header e tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProdutosComponent } from './components/produtos/produtos.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProdutosComponent],
+  imports: [ProdutosComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

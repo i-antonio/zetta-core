e174d2c3b0ee3fa52a145fcbd5c1ed29f94c0ef9
Antonio <a.augusto03@live.com>
2026-04-05 14:57:26 -0300

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProdutosComponent } from './components/produtos/produtos.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProdutosComponent, HeaderComponent],
+  imports: [ProdutosComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
